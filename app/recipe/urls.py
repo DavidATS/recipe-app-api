9dbdb6549a156cea27836de6041b42eb5ac343ed
DavidATS <toro.david@hotmail.com>
2020-05-03 17:51:55 -0500

    Video 63

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -7,6 +7,7 @@ from rest_framework.routers import DefaultRouter  # Registers ulrs for actions
 router = DefaultRouter()
 router.register("tags", views.TagViewSet)
 router.register("ingredients", views.IngredientViewSet)
+router.register("recipes", views.RecipeViewSet)
 
 app_name = "recipe"
 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,4 +1,5 @@
 from core.models import Ingredient
+from core.models import Recipe
 from core.models import Tag
 from recipe import serializers
 from rest_framework import mixins
@@ -31,3 +32,13 @@ class IngredientViewSet(BaseView):
 
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by("-id")
